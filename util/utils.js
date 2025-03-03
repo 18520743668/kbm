506a02186c48813891582a55a73afd07ed8b6882
18520743668 <59762107+18520743668@users.noreply.github.com>
2025-03-03 16:08:04 +0800

    Add files via upload

---
+++ util/utils.js
@@ -0,0 +1,6 @@
+module.exports = {
+    transTime: function(e) {
+        var t = new Date(1e3 * parseInt(e)), n = t.getFullYear(), r = t.getMonth() + 1, a = t.getDate(), s = t.getHours(), g = t.getMinutes(), o = t.getSeconds(), u = new Date();
+        return Date.parse(u.toDateString()), n + "-" + r + "-" + a + " " + s + ":" + g + ":" + o;
+    }
+};
